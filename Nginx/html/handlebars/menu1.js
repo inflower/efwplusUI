c4a97858c8c0e43f5fc70fa84c271c2c01d26d18
343588387 <343588387@qq.com>
2017-03-30 12:57:15 +0800

    code

--- Nginx/html/handlebars/menu1.js
+++ Nginx/html/handlebars/menu1.js
@@ -1,7 +1,7 @@
 ﻿define(['jquery', 'common', "handlebars.min", "text!../../handlebars/menu1.html"], function ($, common, Handlebars, html_template) {
     //
     function showpage(menuId,templates) {
-        common.simpleAjax("Hello/ShowConfig", {}, function (data) {
+        common.simpleAjax("menu/getmenu1", {}, function (data) {
 
             common.loadtemplate(menuId, templates, html_template, { data: common.toJson(data) });
         });
