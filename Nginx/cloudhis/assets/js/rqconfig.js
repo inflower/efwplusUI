6e15429d8bec7875801cc4c11abee68b7eddc285
343588387 <343588387@qq.com>
2017-03-30 16:05:39 +0800

    his

---
+++ Nginx/cloudhis/assets/js/rqconfig.js
@@ -0,0 +1,36 @@
+﻿require.config({
+    baseUrl: 'assets/js',
+    map: {
+        '*': {
+            'css': 'css.min',
+            'text': 'text'
+        }
+    },
+    paths: {
+        //text: 'text',
+        //css: 'css.min',
+        "jquery": 'jquery.min',
+        "jquery.easyui": "../../uiframe/jquery-easyui-1.4.2/jquery.easyui.min",
+        "easyui-lang-zh_CN": "../../uiframe/jquery-easyui-1.4.2/locale/easyui-lang-zh_CN",
+        "amazeui": 'amazeui.min',
+        "jquery.json": 'jquery.json-2.3.min',
+        "common": '../../js/common',
+        "login": "../../js/login",
+        "app":"app",
+        "index": '../../js/index'
+    },
+    shim: {
+        "jquery.easyui": ["jquery", "easyui-lang-zh_CN", "css!../../uiframe/jquery-easyui-1.4.2/themes/bootstrap/easyui.css", "css!../../uiframe/jquery-easyui-1.4.2/themes/icon.css"],
+        "amazeui": ["jquery"],
+        "amazeui.tree.min": ["jquery", "css!../css/amazeui.tree.min.css"],
+        "jquery.cookie": ["jquery"],
+        "jquery.formatDateTime.min": ["jquery"],
+        "jquery.json":["jquery"],
+        "app": ["jquery", "amazeui"],
+        "login": ["amazeui"],
+        "index": ["amazeui", "app", "jquery", "jquery.cookie", "director.min"]
+    },
+    waitSeconds: 50
+});
+
+//requirejs(["amazeui.min","app", "index"]);
---
+++ Nginx/cloudhis/handlebars/setpassword.js
@@ -0,0 +1,23 @@
+﻿define(['jquery', 'common', "jquery.easyui", "handlebars.min", "text!../../handlebars/setpassword.html"], function ($, common, easyui, Handlebars, html_template) {
+
+    function showpage(menuId, templates) {
+        //common.simpleAjax("menu/getmenu4", {}, function (data) {
+        common.loadtemplate(menuId, templates, html_template, { data: [] });
+        $.parser.parse($('#content_body'));
+        //setHeight();
+        //});
+    }
+    function setHeight() {
+        var c = $('#setpassword');
+        var p = c.layout('panel', 'center');    // get the center panel
+        var oldHeight = p.panel('panel').outerHeight();
+        p.panel('resize', { height: 'auto' });
+        var newHeight = p.panel('panel').outerHeight();
+        c.layout('resize', {
+            height: (c.height() + newHeight - oldHeight)
+        });
+    }
+    return {
+        showpage: showpage
+    };
+});
